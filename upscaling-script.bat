af7ad437abc3b5eea462ee5bc764cf0b0182bb0e
ジョセフ <54091798+josephcarragher@users.noreply.github.com>
2023-01-30 22:30:41 -0500

    Update upscaling-script.bat
    cut out unnecessary steps, made file paths variables

--- upscaling-script.bat
+++ upscaling-script.bat
@@ -1,88 +1,72 @@
+@echo off
 GOTO START
-1. pull mp4 from mkv (mp4)
-2. cut up mp4 into images in folder (images)
-3. upscale (waifuoutputs)
-4. build back mp4 from images (upscaledvid)
-4.5. delete images, waifuoutputs
-5. reencode mp4 as mkv (newmkvs)
-6. merge mkvs (merged)
-7. delete files from intermediary folders
+1. cut up mkv into images in folder (images)
+2. upscale (waifuoutputs)
+3. build back mkv from images (upscaledvid)
+3.5. delete images, waifuoutputs
+4. merge mkvs (merged)
+5. delete files from intermediary folders
 
 TODO:
 Make output nice (mute stock, add more clear steps)
-Make all file locations variables for ez pz moving around
 Maybe cut out mkvmerge for ffmpeg
-
 :START
-@echo off
 
 :: >NUL 2>&1
-:: this is for the original mkv folder
-:: set original=C:\Users\Joseph\Downloads\[a4e]Birdy_the_Mighty_OVA
+
+:: Locations for the original file folder, cut images, upscaled images, upscaled video, merged final product, and programs
 set original=C:\Users\Joseph\Documents\upscalingwork\testing
+set images=C:\Users\Joseph\Documents\upscalingwork\images
+set upscaledimg=C:\Users\Joseph\Documents\upscalingwork\waifuoutputs
+set upscaledvid=C:\Users\Joseph\Documents\upscalingwork\upscaledvid
+set final=C:\Users\Joseph\Documents\upscalingwork\merged
+set ffmpegloc=C:\Users\Joseph\Downloads\ffmpeg-2023-01-25-git-2c3107c3e9-full_build\bin
+set mkvmergeloc=C:\Users\Joseph\Downloads\mkvtoolnix
+
 Echo Origin folder: %original%
 echo:
 
-:: 1. mkv to mp4
-Echo Converting from MKV to MP4
 Echo Total file list:
 for %%x in (%original%/*.mkv) do (
-    C:\Users\Joseph\Downloads\ffmpeg-2023-01-25-git-2c3107c3e9-full_build\bin\ffmpeg.exe -i "%original%\%%~nx.mkv" -codec copy "C:\Users\Joseph\Documents\upscalingwork\mp4\%%~nx.mp4"
     echo %%~nx.mkv
 )
 
 echo:
 setlocal enabledelayedexpansion
-:: loop for each video to not mix up the decomposed images. above delayedexpansion is needed to have variables in a loop? idk why this is retarded
-for %%x in (C:\Users\Joseph\Documents\upscalingwork\mp4/*.mp4) do (
-    :: 2. cutting video into images
-    Echo Working on %%~nx
-    ::C:\Users\Joseph\Downloads\ffmpeg-2023-01-25-git-2c3107c3e9-full_build\bin\ffmpeg.exe -i "C:\Users\Joseph\Documents\upscalingwork\mp4\%%~nx.mp4" -qscale:v 1 -qmin 1 -qmax 1 "C:\Users\Joseph\Documents\upscalingwork\images\frame-%%06d.png"
-    C:\Users\Joseph\Downloads\ffmpeg-2023-01-25-git-2c3107c3e9-full_build\bin\ffmpeg.exe -i "C:\Users\Joseph\Documents\upscalingwork\testing\%%~nx.mkv" -qscale:v 1 -qmin 1 -qmax 1 "C:\Users\Joseph\Documents\upscalingwork\images\frame-%%06d.png"
+:: loop for each video to not mix up the decomposed images. above delayedexpansion is needed to have variables in a loop? idk why
+for %%x in (%original%/*.mkv) do (
+    Echo Working on %%~nx.mkv
+    Echo Cutting up frames
+    %ffmpegloc%\ffmpeg.exe -i "%original%\%%~nx.mkv" -qscale:v 1 -qmin 1 -qmax 1 "%images%\frame-%%06d.png" >NUL 2>&1
     
-    :: 3. upscaling
     Echo Upscaling
-    C:\Users\Joseph\Downloads\realesrgan-ncnn-vulkan-20220424-windows\realesrgan-ncnn-vulkan.exe -i "C:\Users\Joseph\Documents\upscalingwork\images" -o "C:\Users\Joseph\Documents\upscalingwork\waifuoutputs" -n realesr-animevideov3 -s 2 -g 1
+    C:\Users\Joseph\Downloads\realesrgan-ncnn-vulkan-20220424-windows\realesrgan-ncnn-vulkan.exe -i "%images%" -o "%upscaledimg%" -n realesr-animevideov3 -s 2 -g 1 >NUL 2>&1
 
-    :: 4. rebuilding upscaled images to video
-    Echo Rebuilding into video
     :: this requires knowing the fps of the video. batch cannot in herently do floats so after ffprobe gets the fps fraction, powershell is used to get the decimal
-    for /F "delims=" %%L in ('C:\Users\Joseph\Downloads\ffmpeg-2023-01-25-git-2c3107c3e9-full_build\bin\ffprobe.exe -v error -select_streams v -of default^=noprint_wrappers^=1:nokey^=1 -show_entries stream^=r_frame_rate "C:\Users\Joseph\Documents\upscalingwork\mp4\%%~nx.mp4"') do (set "fps=%%L")
+    for /F "delims=" %%L in ('%ffmpegloc%\ffprobe.exe -v error -select_streams v -of default^=noprint_wrappers^=1:nokey^=1 -show_entries stream^=r_frame_rate "%original%\%%~nx.mkv"') do (set "fps=%%L") >NUL 2>&1
     for /f "delims=" %%a in ('powershell -Command !fps!') do set result=%%a
+
     :: now that fps is found, ffmpeg can rebuild the video
     Echo Calculated fps: !result!
-    C:\Users\Joseph\Downloads\ffmpeg-2023-01-25-git-2c3107c3e9-full_build\bin\ffmpeg.exe -r !result! -i "C:\Users\Joseph\Documents\upscalingwork\waifuoutputs\frame-%%06d.png" -c:v libx264 -r !result! -pix_fmt yuv420p "C:\Users\Joseph\Documents\upscalingwork\upscaledvid\%%~nx.mp4"
+    Echo Rebuilding into video
+    %ffmpegloc%\ffmpeg.exe -r !result! -i "%upscaledimg%\frame-%%06d.png" -c:v libx264 -r !result! -pix_fmt yuv420p "%upscaledvid%\%%~nx.mkv" >NUL 2>&1
 
-    :: 4.5 deleting items from intermediary image related folders
+    Echo %%~nx.mkv upscaled!
     Echo Deleting intermediary items
-    cd C:\Users\Joseph\Documents\upscalingwork\waifuoutputs
+    cd %images%
     del *.png
-    cd C:\Users\Joseph\Documents\upscalingwork\images
+    cd %upscaledimg%
     del *.png
     echo:
 )
-endlocal
+endlocal >NUL 2>&1 REM for some reason this sometimes spits out an error, sometimes not. so im muting it
 
-::set original=C:\Users\Joseph\Documents\upscalingwork\testing
-
-:: 5. mp4 to mkv
-Echo Convering all upscaled mp4 to mkv
-:: idk why this sometimes makes the first file all lowercase, but in theory windows is case insensitive so...
-for %%x in (C:\Users\Joseph\Documents\upscalingwork\upscaledvid/*.mp4) do C:\Users\Joseph\Downloads\ffmpeg-2023-01-25-git-2c3107c3e9-full_build\bin\ffmpeg.exe -i "C:\Users\Joseph\Documents\upscalingwork\upscaledvid\%%~nx.mp4" -codec copy "C:\Users\Joseph\Documents\upscalingwork\newmkvs\%%~nx.mkv"
-
-:: 6. this loops through the original folder and newmkvs, merging them and storing into merged
 Echo Merging MKVs
-for %%x in (%original%/*.mkv) do "C:\Users\Joseph\Downloads\mkvtoolnix\mkvmerge.exe" -o "C:\Users\Joseph\Documents\upscalingwork\merged\%%~nx.mkv" -D ( "%original%\%%~nx.mkv" ) -A -S -T -M -B --no-chapters ( "C:\Users\Joseph\Documents\upscalingwork\newmkvs\%%~nx.mkv" )
+for %%x in (%original%/*.mkv) do "%mkvmergeloc%\mkvmerge.exe" -o "%final%\%%~nx.mkv" -D ( "%original%\%%~nx.mkv" ) -A -S -T -M -B --no-chapters ( "%upscaledvid%\%%~nx.mkv" ) >NUL 2>&1
 
-:: 7. deleting leftover items from intermediary folders
 Echo Deleting intermediary items
-cd C:\Users\Joseph\Documents\upscalingwork\mp4
-del *.mp4
-cd C:\Users\Joseph\Documents\upscalingwork\newmkvs
+cd %upscaledvid%
 del *.mkv
-cd C:\Users\Joseph\Documents\upscalingwork\upscaledvid
-del *.mp4
-
 
 Echo Done
 pause
